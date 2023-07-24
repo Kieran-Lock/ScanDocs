79c1a276a8c7fcb588fbfbac99e332f0d32a58c2
Kieran-Lock <Kieran.Lock@outlook.com>
2023-07-24 23:48:11 +0100

    Added source code block and navigation

--- scandocs/templates/routes/+page.svelte
+++ scandocs/templates/routes/+page.svelte
@@ -1,5 +1,17 @@
 <script>
     import {activeProject} from "$lib/stores/project";
+    import {CodeBlock} from "@skeletonlabs/skeleton";
 </script>
 
-<h1>{activeProject.getActive($activeProject).meta.name}</h1>
+<div class="flex w-full justify-center items-center">
+    <div class="card m-8 flex flex-col gap-4 items-center justify-center w-[50%] p-4">
+        <header>
+            <h1 class="text-2xl font-bold">{activeProject.getActive($activeProject).meta.name}</h1>
+        </header>
+        <section>
+            {#if activeProject.getActive($activeProject).meta.source}
+                <CodeBlock class="px-8" language="Python" code={activeProject.getActive($activeProject).meta.source}></CodeBlock>
+            {/if}
+        </section>
+    </div>
+</div>
