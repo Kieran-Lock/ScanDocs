b1b63c16bea065bb9648fd26ca379c42e879580c
Kieran-Lock <Kieran.Lock@outlook.com>
2023-07-26 01:30:05 +0100

    Fixed exceptions block showing bug

--- scandocs/templates/lib/components/RaisesBlock.svelte
+++ scandocs/templates/lib/components/RaisesBlock.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import {activeProject} from "$lib/stores/project";
-    import {onDestroy} from "svelte";
+    import {onDestroy, onMount} from "svelte";
 
     let meta
     const unsubscribe = activeProject.subscribe((value) => {
@@ -10,7 +10,7 @@
     onDestroy(unsubscribe);
 </script>
 
-{#if meta.exceptions}
+{#if meta.exceptions && meta.exceptions.length}
     <div class="flex flex-col gap-2 card w-[50%] p-4">
         <header>
             <h2 class="text-2xl font-bold">Raises</h2>
