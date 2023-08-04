0c6a44ff1cca4e1870dd00fb19c5dfa7ab49c6a5
Kieran-Lock <Kieran.Lock@outlook.com>
2023-08-05 00:03:12 +0100

    Added deprecation notice block

--- scandocs/templates/lib/components/blocks/DeprecationBlock.svelte
+++ scandocs/templates/lib/components/blocks/DeprecationBlock.svelte
@@ -1,3 +1,10 @@
 <script lang="ts">
     export let deprecation
 </script>
+
+<div class="flex flex-col card p-4 gap-4 variant-glass-warning">
+    <header>
+        <h2>Deprecated since {deprecation.version}</h2>
+    </header>
+    <p>{deprecation.description}</p>
+</div>
