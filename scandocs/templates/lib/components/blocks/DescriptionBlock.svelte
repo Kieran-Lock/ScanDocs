7ffa30877a8d50604c8f1d7dca6f1c6aef5848e9
Kieran-Lock <Kieran.Lock@outlook.com>
2023-08-04 15:32:46 +0100

    Re-padded description block

--- scandocs/templates/lib/components/blocks/DescriptionBlock.svelte
+++ scandocs/templates/lib/components/blocks/DescriptionBlock.svelte
@@ -4,12 +4,12 @@
     export let long
 </script>
 
-<div class="flex flex-col gap-2 card p-4 variant-glass-surface">
+<div class="flex flex-col card p-4 gap-4 variant-glass-surface">
     <header class="flex flex-row justify-between items-center w-full">
         <h2 class="text-2xl font-bold">{name}</h2>
     </header>
     {#if short || long}
-        <section class="w-full pt-2">
+        <section class="w-full pt-2 flex flex-col gap-2">
             {#if short}
                 <p>{short}</p>
             {/if}
