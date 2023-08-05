9be0a28180eb4a6c1d3bc96fc22fe574c9f648f1
Kieran-Lock <Kieran.Lock@outlook.com>
2023-08-05 17:55:48 +0100

    Added all inbuilt themes for customization

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from scandocs import Package, Documentation, Configuration
+from scandocs import Package, Documentation, Configuration, Themes
 from pathlib import Path
 from pprint import pprint
 import test_project
 
 
 project = Package.from_module(test_project)
-docs = Documentation(project, Path("./docs"), Configuration("Test Project Title"))
+docs = Documentation(project, Path("./docs"), Configuration("Test Project Title", theme=Themes.GOLD_NOUVEAU))
 pprint(project.serialize(docs.filter).to_json())
 docs.output()
--- scandocs/documentation/themes.py
+++ scandocs/documentation/themes.py
@@ -3,3 +3,11 @@ from enum import Enum
 
 class Themes(Enum):
     SKELETON = "skeleton"
+    MODERN = "modern"
+    ROCKET = "rocket"
+    SEAFOAM = "seafoam"
+    VINTAGE = "vintage"
+    SAHARA = "sahara"
+    HAMLINDIGO = "hamlindigo"
+    GOLD_NOUVEAU = "gold-nouveau"
+    CRIMSON = "crimson"
