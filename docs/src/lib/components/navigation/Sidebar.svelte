a524f7c53f676ace774a8313ae50b9c66862d360
Kieran-Lock <Kieran.Lock@outlook.com>
2023-08-13 22:43:57 +0100

    Improved sidebar responsiveness

--- docs/src/lib/components/navigation/Sidebar.svelte
+++ docs/src/lib/components/navigation/Sidebar.svelte
@@ -7,7 +7,7 @@
     import {projectName} from "$lib/content/configuration";
 </script>
 
-<div class="flex flex-col card p-4 h-full gap-8 overflow-y-scroll hide-scrollbar">
+<div class="flex flex-col card p-4 h-full gap-8 overflow-y-scroll hide-scrollbar md:min-w-[320px] xl:min-w-[420px]">
     <header class="flex flex-col card justify-center items-center p-4 gap-6 variant-ringed-surface pb-6">
         <button type="button" on:click={selectNodeBuilder(project)}>
             <span class="text-2xl font-bold">{projectName}</span>
