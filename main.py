bd4dceff20154e168595541195fe1c2eca0b0eaf
Kieran-Lock <Kieran.Lock@outlook.com>
2023-08-13 16:02:18 +0100

    Update docs to latest version

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from scandocs import Package, Documentation, Configuration, Themes
 from pathlib import Path
-from pprint import pprint
 import scandocs
 
 
 project = Package.from_module(scandocs)
 docs = Documentation(project, Path("./docs"), Configuration("ScanDocs", theme=Themes.GOLD_NOUVEAU))
-pprint(project.serialize(docs.filter).to_json())
 docs.output()
